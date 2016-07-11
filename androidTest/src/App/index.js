1824723841e02b5dd084abee0188df90427b919c
Richard Izip <richardizip@gmail.com>
2016-07-11 15:39:15 +0200

    add back button route action

--- androidTest/src/App/index.js
+++ androidTest/src/App/index.js
@@ -6,7 +6,8 @@ import {
   Text,
   View,
   Navigator,
-  TouchableOpacity
+  TouchableOpacity,
+  BackAndroid
 } from 'react-native';
 
 import WelcomeScreen from '../WelcomeScreen';
@@ -17,11 +18,24 @@ import User from '../Users/User';
 import NoPage from '../NoPage';
 import styles from './styles';
 
+let _navigator;
+
+// BackAndroid is a simple library that binds to the 
+// native events of the host device
+BackAndroid.addEventListener('hardwareBackPress', function() {
+if (_navigator.getCurrentRoutes().length === 1  ) {
+  return false;
+}
+_navigator.pop();
+return true;
+});
+
 class App extends Component {
   
   // used by navigator to render scene for given route
   renderScene(route, navigator) {
     const routeId = route.id;
+     _navigator = navigator
     switch (routeId) {
       case 'WelcomeScreen':
         return (
